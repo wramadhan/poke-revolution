79775127b78dd2d438c29a05a25a0169ff786f19
wahyu.ramadhan <wahyu.ramadhan@tapera.go.id>
2024-02-09 18:05:27 +0700

    add : padding

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import { Icon } from '@iconify/react'
 
 export const Footer = () => {
   return (
-    <footer className='px-12 bg-tertiary'>
+    <footer className='px-12 py-2 bg-tertiary'>
       <div className='flex justify-between text-quaternary'>
         <span className='flex'>
           <Icon icon='ic:twotone-catching-pokemon' className='text-4xl' />
