400854e9a1c5184fd700eb270b1d71bc02d4d3da
wahyu.ramadhan <wahyu.ramadhan@tapera.go.id>
2024-02-09 18:07:29 +0700

    add : facility section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Footer } from '@/components/footer'
+import { Facility } from '@/components/home/facility'
 import { MainHome } from '@/components/home/mainSection'
 export default function Home() {
   return (
     <div className='font-poppins'>
       <MainHome />
+      <Facility />
       <Footer />
     </div>
   )
