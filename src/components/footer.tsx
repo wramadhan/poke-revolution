27a53d642fbe9914c85f1238624dcc206b3c12e5
wahyu.ramadhan <wahyu.ramadhan@tapera.go.id>
2024-02-15 18:12:29 +0700

    add : route to home at logo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,22 +2,25 @@ import { HeroSubTitle } from '@/services/home/hero'
 import { Icon } from '@iconify/react'
 import { useLangStore } from '@/store/language'
 import { ContactUs } from '@/services/global/footer'
+import Link from 'next/link'
 
 export const Footer = () => {
   const { lang } = useLangStore()
   return (
     <footer className='smMax:px-2 px-12 py-2 bg-tertiary'>
       <div className='flex justify-between text-quaternary'>
-        <span className='flex'>
-          <Icon
-            icon='ic:twotone-catching-pokemon'
-            className='text-4xl rotate-12 mt-2'
-          />
-          <h1 className='font-mochiypopone pt-1.5'>
-            <div>Poke Revolution</div>
-            <div className='text-xs'>{HeroSubTitle[lang ? 'en' : 'id']}</div>
-          </h1>
-        </span>
+        <Link href={'/'}>
+          <span className='flex'>
+            <Icon
+              icon='ic:twotone-catching-pokemon'
+              className='text-4xl rotate-12 mt-2'
+            />
+            <h1 className='font-mochiypopone pt-1.5'>
+              <div>Poke Revolution</div>
+              <div className='text-xs'>{HeroSubTitle[lang ? 'en' : 'id']}</div>
+            </h1>
+          </span>
+        </Link>
         <button>{ContactUs[lang ? 'en' : 'id']}</button>
       </div>
       <p className='text-white sm:text-center pt-2'>
