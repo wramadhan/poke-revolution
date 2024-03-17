428ce339244086e0fcafad091e07e089819e4278
wahyu.ramadhan <wahyu.ramadhan@tapera.go.id>
2024-03-17 16:40:58 +0700

    add : condition route

--- src/components/scrollToTopButton.tsx
+++ src/components/scrollToTopButton.tsx
@@ -1,4 +1,5 @@
 import { useLangStore } from '@/store/language'
+import { useRouter } from 'next/router'
 
 export const ScrollToTopButton = ({
   scrollToTop,
@@ -6,27 +7,32 @@ export const ScrollToTopButton = ({
   scrollToTop: () => void
 }) => {
   const { lang } = useLangStore()
-  return (
-    <button
-      onClick={scrollToTop}
-      id='btn-back-to-top'
-      title={lang ? 'Back to Top' : 'Kembali ke atas'}
-      className={`fixed bottom-8 right-8 bg-secondary text-primary border-tertiary bg-gray-700 hover:bg-gray-800 p-2 rounded-full drop-shadow-md shadow-tertiary`}
-    >
-      <svg
-        className='w-6 h-6'
-        fill='none'
-        stroke='currentColor'
-        viewBox='0 0 24 24'
-        xmlns='http://www.w3.org/2000/svg'
+  const router = useRouter()
+  if (router.pathname == '/') {
+    return (
+      <button
+        onClick={scrollToTop}
+        id='btn-back-to-top'
+        title={lang ? 'Back to Top' : 'Kembali ke atas'}
+        className={`fixed bottom-8 right-8 bg-secondary text-primary border-tertiary bg-gray-700 hover:bg-gray-800 p-2 rounded-full drop-shadow-md shadow-tertiary`}
       >
-        <path
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          strokeWidth='2'
-          d='M5 10l7-7m0 0l7 7m-7-7v18'
-        ></path>
-      </svg>
-    </button>
-  )
+        <svg
+          className='w-6 h-6'
+          fill='none'
+          stroke='currentColor'
+          viewBox='0 0 24 24'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='2'
+            d='M5 10l7-7m0 0l7 7m-7-7v18'
+          ></path>
+        </svg>
+      </button>
+    )
+  } else {
+    return <></>
+  }
 }
